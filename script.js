301ae28d0bdc7d345b024abe45bf93eb61a2eac9
Vips43 <vipulgehlot43@gmail.com>
2025-08-13 14:31:20 +0530

    de

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ hireMeBtn.addEventListener('click',()=>{
 
 // design about-us section
 
+// gsap.from('.subtitle',{
+//     y:-50,
+//     scale:0,
+//     opacity:0,
+//     duration:1,
+// })
+// gsap.from('.title-span',{
+//     x:50,
+//     color:"grey",
+//     duration:2,
+// })
+gsap.from('.skills-container .skills', {
+    scale: 0,
+    opacity:0,
+    y: -30,
+    delay: .5,
+    markers: true,
+    start: 'top 55%',
+    end: 'top 40%',
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.skills-container .skills',
+        scroller: 'body',
+        start: 'top 55%',
+        end: 'top 40%',
+        scrub: 2,
+        pin:true
+    }
+})
