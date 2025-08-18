c8eea6e92173ea9e1cfb27ec4997a91867f2215a
Vips43 <vipulgehlot43@gmail.com>
2025-08-18 13:31:35 +0530

    de

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
 let hireMeBtn = document.querySelector('.hire-me')
 let alertMsg = document.querySelector('.alert-msg')
-let about = document.querySelector('.about')
+const about1 = document.querySelector('.about:nth-child(1)')
+const about2 = document.querySelector('.about:nth-child(2)')
 
-hireMeBtn.addEventListener('click',()=>{
-    alertMsg.style.opacity = '1'
-    setTimeout(() => {
-        alertMsg.style.opacity = '0'
-    }, 2000);
+hireMeBtn.addEventListener('click', () => {
+  alertMsg.style.opacity = '1'
+  setTimeout(() => {
+    alertMsg.style.opacity = '0'
+  }, 2000);
 
-    
+
 })
 
 // skills section design
 
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".skills .skill");
-    const io = new IntersectionObserver((entries) => {
-      entries.forEach(e => { if (e.isIntersecting) e.target.classList.add("show"); });
-    }, { threshold: 0.1 });
-    cards.forEach(c => io.observe(c));
-  });
+const cards = document.querySelectorAll(".skills .skill");
+const contactsMeP = document.querySelector('.contacts-me p')
+const contactsMeH2 = document.querySelector('.contacts-me h2')
+const contactMe = document.querySelector('.contact-me')
+const number = document.querySelector('.number')
+const aboutImg = document.querySelector('.about-img')
 
-// design about-us section
 
-// gsap.from('.subtitle',{
-//     y:-50,
-//     scale:0,
-//     opacity:0,
-//     duration:1,
-// })
-// gsap.from('.title-span',{
-//     x:50,
-//     color:"grey",
-//     duration:2,
-// })
-// gsap.from('.skills-container .skills', {
-//     scale: 0,
-//     opacity:0,
-//     y: -30,
-//     delay: .5,
-//     markers: true,
-//     // start: 'top 55%',
-//     // end: 'bottom 40px',
-//     duration: 1,
-//     scrollTrigger: {
-//         trigger: '.skills-container .skills',
-//         scroller: 'body',
-//         start: 'top 55%',
-//         end: 'top 40%',
-//         scrub: 2,
-//         pin:true
-//     }
-// })
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove("show")
+    }
+
+  })
+}, {
+  rootMargin: '-80px'
+})
+
+cards.forEach(e => observer.observe(e))
+
+const arr = [contactsMeP, contactsMeH2, contactMe, number, aboutImg, about1, about2]
+arr.forEach(e => observer.observe(e))
+console.log(arr);
+
+const projectLi = document.querySelectorAll('.project-li')
+projectLi.forEach(el => observer.observe(el))
