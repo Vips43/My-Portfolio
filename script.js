ba8626c16929238d5c7e5f65601c702c804d5f92
Vips43 <vipulgehlot43@gmail.com>
2025-08-18 13:41:25 +0530

    de

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 let hireMeBtn = document.querySelector('.hire-me')
 let alertMsg = document.querySelector('.alert-msg')
-const about1 = document.querySelector('.about:nth-child(1)')
-const about2 = document.querySelector('.about:nth-child(2)')
-
 hireMeBtn.addEventListener('click', () => {
   alertMsg.style.opacity = '1'
   setTimeout(() => {
     alertMsg.style.opacity = '0'
   }, 2000);
-
-
 })
 
-// skills section design
-
-const cards = document.querySelectorAll(".skills .skill");
+//adding scroll animation
+const about1 = document.querySelector('.about:nth-child(1)')
+const about2 = document.querySelector('.about:nth-child(2)')
+const skills = document.querySelectorAll(".skills .skill");
 const contactsMeP = document.querySelector('.contacts-me p')
 const contactsMeH2 = document.querySelector('.contacts-me h2')
 const contactMe = document.querySelector('.contact-me')
 const number = document.querySelector('.number')
 const aboutImg = document.querySelector('.about-img')
-
-
-
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -37,7 +30,7 @@ const observer = new IntersectionObserver((entries) => {
   rootMargin: '-80px'
 })
 
-cards.forEach(e => observer.observe(e))
+skills.forEach(e => observer.observe(e))
 
 const arr = [contactsMeP, contactsMeH2, contactMe, number, aboutImg, about1, about2]
 arr.forEach(e => observer.observe(e))
